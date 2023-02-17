ab041887de4cebc5ecb26530d132eab9ddf843fd
Villalba Julian <96155980+wulfiss@users.noreply.github.com>
2023-02-16 21:46:01 -0300

    Refactor elment creation

--- src/modules/UI.js
+++ src/modules/UI.js
@@ -1,47 +1,12 @@
 import { UI, DATA } from "./dataInputs";
 
-const createElements = function littleHelperForCreatingHtmlElements(type, id, clasS, attrType, textNode, idLbl, attrValue, attrName, placeHldr, randomAttr, randomValue){
-    const temp = document.createElement(type);
-
-    if(id){
-        temp.setAttribute('id', id);
-    }
-    if(clasS){
-        temp.classList.add(...clasS);
-    }
-    if(attrType){
-        temp.setAttribute('type', attrType);
-    }
-    if(textNode){
-        const txt = document.createTextNode(textNode);
-        temp.appendChild(txt);
-    }
-    if(idLbl){
-        temp.htmlFor = idLbl;
-    }
-    if(attrValue){
-        temp.setAttribute('value', attrValue);
-    }
-    if(attrName){
-        temp.setAttribute('name', attrName);
-    }
-    if(placeHldr){
-        temp.setAttribute('placeholder', placeHldr);
-    }
-    if(randomAttr){
-        temp.setAttribute(randomAttr, randomValue);
-    }
-
-    return temp;
-}
-
 const addAttr = (el, attr) => {
     for(const [key, value] of Object.entries(attr)){
         el.setAttribute(`${key}`, `${value}`);
     }
 }
 
-const createElementsTwo = function littleHelperForCreatingHtmlElements(node, txt, attr){
+const createElements = function littleHelperForCreatingHtmlElements(node, txt, attr){
 
     const temp = document.createElement(node);
 
@@ -59,41 +24,41 @@ const createElementsTwo = function littleHelperForCreatingHtmlElements(node, txt
 }
 
 const form = function CreateAFormForAddTheBook(){
-    const fieldset = createElementsTwo('fieldset');
-    const legend = createElementsTwo('legend', 'Book');
+    const fieldset = createElements('fieldset');
+    const legend = createElements('legend', 'Book');
 
-    const form = createElementsTwo('form');
-    const ulForm = createElementsTwo('ul');
+    const form = createElements('form');
+    const ulForm = createElements('ul');
 
-    const liFormOne = createElementsTwo('li');
-    const bookName = createElementsTwo('input', '', { id: 'nameBook', type: 'text', placeHolder: 'Title' });
+    const liFormOne = createElements('li');
+    const bookName = createElements('input', '', { id: 'nameBook', type: 'text', placeHolder: 'Title' });
     liFormOne.appendChild(bookName);
 
-    const liFormTwo = createElementsTwo('li');
-    const bookAuthor = createElementsTwo('input', '', { id: 'authorBook', type: 'text', placeHolder: 'Author' });
+    const liFormTwo = createElements('li');
+    const bookAuthor = createElements('input', '', { id: 'authorBook', type: 'text', placeHolder: 'Author' });
     liFormTwo.appendChild(bookAuthor);
 
     /* when these element are onfocus they are showed as input type date, but when they lose focus they are showed as type text, so they can display the placeholder*/
     const liFromThree = createElements('li');
-    const initialDate = createElementsTwo('input', '', { id: 'initialDate', type: 'text', placeHolder: 'Start Date', onfocus: '(this.type="date")', onblur: '(this.type="text")' });
+    const initialDate = createElements('input', '', { id: 'initialDate', type: 'text', placeHolder: 'Start Date', onfocus: '(this.type="date")', onblur: '(this.type="text")' });
     liFromThree.appendChild(initialDate);
 
     const liFromFour = createElements('li');
-    const finalDate = createElementsTwo('input', '', { id: 'finalDate', type: 'text', placeHolder: 'Finish Date', onfocus: '(this.type="date")', onblur: '(this.type="text")' });
+    const finalDate = createElements('input', '', { id: 'finalDate', type: 'text', placeHolder: 'Finish Date', onfocus: '(this.type="date")', onblur: '(this.type="text")' });
     liFromFour.appendChild(finalDate);
 
     /************************ create select element in form  ************************/
     const liFormFive = createElements('li');
-    const statusOp = createElementsTwo('select', '', { id: 'statusBook', required: '' });
+    const statusOp = createElements('select', '', { id: 'statusBook', required: '' });
 
     // add this part in order to mimic placeholder follow in the css
-    const placeHldrStatus = createElementsTwo('option', 'Status', { id:'Status', class: 'options', disabled: '', selected: '', value: '' });
+    const placeHldrStatus = createElements('option', 'Status', { id:'Status', class: 'options', disabled: '', selected: '', value: '' });
     statusOp.appendChild(placeHldrStatus);
 
     const optArr = { reading: 'Reading', completed: 'Completed', onhold: 'On-Hold', dropped: 'Dropped', planTwoWatch: 'Plan to Read' };
 
     for (const [key, value] of Object.entries(optArr)){
-        const options = createElementsTwo('option', value, { id: key, class: 'options', value: value });
+        const options = createElements('option', value, { id: key, class: 'options', value: value });
         statusOp.appendChild(options);
     }
 
@@ -102,8 +67,8 @@ const form = function CreateAFormForAddTheBook(){
     /************************ create select element in form  ************************/
 
     const liFormBtn = createElements('li');
-    const btnFormSave = createElementsTwo('button', 'Save', { id: 'btnSave', class: 'buttons', type: 'button' });
-    const btnFormCancel = createElementsTwo('button', 'Cancel', { id: 'btnCancel', class: 'buttons', type: 'button' });
+    const btnFormSave = createElements('button', 'Save', { id: 'btnSave', class: 'buttons', type: 'button' });
+    const btnFormCancel = createElements('button', 'Cancel', { id: 'btnCancel', class: 'buttons', type: 'button' });
     liFormBtn.appendChild(btnFormSave);
     liFormBtn.appendChild(btnFormCancel);
 
@@ -124,18 +89,18 @@ const form = function CreateAFormForAddTheBook(){
 }
 
 const showTableContent = () => {
-    const content = createElements('div', 'tableContent');
+    const content = createElements('div', '', { id:'tableContent' });
     const arrBooks = JSON.parse(localStorage.getItem('arrBooks'));
 
     if(arrBooks){
         for(let i = 0; i < arrBooks.length; i += 1){
-            const bookItem = createElements('div', 'bookDiv', ['bookItems']);
+            const bookItem = createElements('div', '', { id:'bookDiv', class: 'bookItems' });
             bookItem.dataset.book = i;
 
-            const bookItemTitle = createElements('div', 'bookItemTitle', ['bookSubItems'], '', `${arrBooks[i]['title']}`);
-            const bookItemAuthor = createElements('div', 'bookItemAuthor', ['bookSubItems'], '', `${arrBooks[i]['author']}`);
-            const bookItemStartDate = createElements('div', 'bookItemStartDate', ['bookSubItems'], '', `${arrBooks[i]['startDate']}`);
-            const bookItemFinishDate = createElements('div', 'bookItemFinishDate', ['bookSubItems'], '', `${arrBooks[i]['finishDate']}`);
+            const bookItemTitle = createElements('div', arrBooks[i]['title'], { id: 'bookItemTitle', class: 'bookSubItems' });
+            const bookItemAuthor = createElements('div', arrBooks[i]['author'], { id: 'bookItemAuthor', class: 'bookSubItems' });
+            const bookItemStartDate = createElements('div', arrBooks[i]['startDate'], { id: 'bookItemStartDate', class: 'bookSubItems' });
+            const bookItemFinishDate = createElements('div', arrBooks[i]['finishDate'], { id: 'bookItemFinishDate', class: 'bookSubItems' });
 
             bookItem.appendChild(bookItemTitle);
             bookItem.appendChild(bookItemAuthor);
@@ -151,13 +116,13 @@ const showTableContent = () => {
 }
 
 const bookTable = () => {
-    const tableMain = createElements('div', 'tableMain');
+    const tableMain = createElements('div', '', { id:'tableMain' });
 
-    const header = createElements('div', 'tableHeader');
-    const headerTitle = createElements('div', 'headerTitle', ['headerItems'], '','Title');
-    const headerAuthor = createElements('div', 'headerAuthor', ['headerItems'], '', 'Author');
-    const headerStarDate = createElements('div', 'headerStarDate', ['headerItems'], '', 'Start date');
-    const headerFinishDate = createElements('div', 'headerFinishDate', ['headerItems'], '', 'Finish date');
+    const header = createElements('div', '', { id: 'tableHeader' });
+    const headerTitle = createElements('div', 'Title', { id: 'headerTitle', class: 'headerItems' });
+    const headerAuthor = createElements('div', 'Author', { id: 'headerAuthor', class: 'headerItems' });
+    const headerStarDate = createElements('div', 'Start date', { id: 'headerStarDate', class: 'headerItems' });
+    const headerFinishDate = createElements('div', 'Finish date', { id: 'headerFinishDate', class: 'headerItems' });
 
     header.appendChild(headerTitle);
     header.appendChild(headerAuthor);
@@ -172,49 +137,46 @@ const bookTable = () => {
 
 const windowInfo = (indexBook) => {
     const { arrBooks } = DATA();
-    const mainInfoDiv = createElements('div', 'mainInfoDiv');
+    const mainInfoDiv = createElements('div', '', { id: 'mainInfoDiv' });
 
     const ulInfoDiv = createElements('ul');
 
     const liInfoOne = createElements('li');
-    const titleInfo = createElements('p', 'titleInfo','','','Title:');
-    const titleInfoInput = createElements('input', 'titleInfoInput', '', 'text', '', '', arrBooks[indexBook].title);
+    const titleInfo = createElements('p', 'Title:', { id:'titleInfo' });
+    const titleInfoInput = createElements('input', '', { id: 'titleInfoInput', type: 'text', value: arrBooks[indexBook].title });
 
     liInfoOne.appendChild(titleInfo);
     liInfoOne.appendChild(titleInfoInput);
     ulInfoDiv.appendChild(liInfoOne);
 
     const liInfoTwo = createElements('li');
-    const authorInfo = createElements('p', 'authorInfo','','','Author:');
-    const authorInfoInput = createElements('input', 'authorInfoInput', '','text', '', '', arrBooks[indexBook].author);
+    const authorInfo = createElements('p', 'Author:', { id:'authorInfo' });
+    const authorInfoInput = createElements('input', '', { id: 'authorInfoInput', type: 'text', value: arrBooks[indexBook].author });
 
     liInfoTwo.appendChild(authorInfo);
     liInfoTwo.appendChild(authorInfoInput);
     ulInfoDiv.appendChild(liInfoTwo);
 
     const liInfoThree = createElements('li');
-    const startInfo = createElements('p', 'startInfo','','','Start Date:');
-    const startInfoInput = createElements('input', 'startInfoInput', '','text', '', '', arrBooks[indexBook].startDate);
-    startInfoInput.setAttribute('onfocus', '(this.type="date")'); // show placeholder in date input
-    startInfoInput.setAttribute('onblur', '(this.type="text")');// show placeholder in date input
+    const startInfo = createElements('p', 'Start Date:', { id:'startInfo' });
+    const startInfoInput = createElements('input', '', { id: 'startInfoInput', type: 'text', onfocus: '(this.type="date")', onblur: '(this.type="text")', value: arrBooks[indexBook].startDate });
 
     liInfoThree.appendChild(startInfo);
     liInfoThree.appendChild(startInfoInput);
     ulInfoDiv.appendChild(liInfoThree);
 
     const liInfoFour = createElements('li');
-    const finishInfo = createElements('p', 'finishInfo','','','Finish Date:');
-    const finishInfoInput = createElements('input', 'finishInfoInput', '','text', '', '', arrBooks[indexBook].finishDate);
-    finishInfoInput.setAttribute('onfocus', '(this.type="date")'); // show placeholder in date input
-    finishInfoInput.setAttribute('onblur', '(this.type="text")');// show placeholder in date input
+    const finishInfo = createElements('p', 'Finish Date:', { id:'finishInfo' });
+    const finishInfoInput = createElements('input', '', { id: 'finishInfoInput', type: 'text', onfocus: '(this.type="date")', onblur: '(this.type="text")', value: arrBooks[indexBook].finishDate });
+
 
     liInfoFour.appendChild(finishInfo);
     liInfoFour.appendChild(finishInfoInput);
     ulInfoDiv.appendChild(liInfoFour);
 
     const liInfoFive = createElements('li');
-    const btnInfoSave = createElements('button', 'btnInfoSave', ['btnInfo'], 'button', 'Save and Close');
-    const btnInfoClose = createElements('button', 'btnInfoClose', ['btnInfo'], 'button', 'Close');
+    const btnInfoSave = createElements('button', 'Save and Close', { id: 'btnInfoSave', class: 'btnInfo', type: 'button' });
+    const btnInfoClose = createElements('button', 'Close', { id: 'btnInfoClose', class: 'btnInfo', type: 'button' });
 
     liInfoFive.appendChild(btnInfoSave);
     liInfoFive.appendChild(btnInfoClose);
