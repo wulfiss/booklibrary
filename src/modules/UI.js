f7863854b8f9d031cf278a2a55680fd70d587739
Villalba Julian <96155980+wulfiss@users.noreply.github.com>
2023-02-22 20:26:53 -0300

    Add btn edit & add hover to appear

--- src/modules/UI.js
+++ src/modules/UI.js
@@ -97,14 +97,20 @@ const showTableContent = () => {
             const bookItemStartDate = createElements({ node: 'div', text: arrBooks[i]['startDate'], id: 'bookItemStartDate', class: 'bookSubItems' });
             const bookItemFinishDate = createElements({ node: 'div', text: arrBooks[i]['finishDate'], id: 'bookItemFinishDate', class: 'bookSubItems' });
             const bookItemStatus = createElements({ node: 'div', text: arrBooks[i]['status'], id: 'bookItemStatus', class: 'bookSubItems' });
+
+            const bookItemBTN = createElements({ node: 'div', id: 'bookItemBTN', class: 'bookSubItems subItemButtons' });
             const btnDelete = createElements({ node: 'button', type: 'button', id: 'btnDelete', class: 'buttons', text: 'Delete', 'data-book': i, 'data-btn': 'btnDelete' });
+            const btnEdit = createElements({ node: 'button', type: 'button', id: 'btnEdit', class: 'buttons', text: 'Edit', 'data-book': i, 'data-btn': 'btnEdit' });
+
+            bookItemBTN.appendChild(btnDelete);
+            bookItemBTN.appendChild(btnEdit);
 
             bookItem.appendChild(bookItemTitle);
             bookItem.appendChild(bookItemAuthor);
             bookItem.appendChild(bookItemStatus);
             bookItem.appendChild(bookItemStartDate);
             bookItem.appendChild(bookItemFinishDate);
-            bookItem.appendChild(btnDelete);
+            bookItem.appendChild(bookItemBTN);
             content.appendChild(bookItem);
         }
     }
